4a441e7fac6be83d20a5f9b428031294d6fd527a
jaisingh-25 <113584679+jaisingh-25@users.noreply.github.com>
2023-09-07 02:23:37 +0530

    Update 11_unnamed_1.py

--- 11_unnamed_1.py
+++ 11_unnamed_1.py
@@ -17,4 +17,4 @@ print(sum)
 # #List Comprehension short code
 # import re
 # #a=open(r"...\11_3_unnamed.txt")
-# print(sum([int(i) for i in re.findall("[0-9]+", open(r"C:\Users\jaisi\Documents\Jai\Coursera\Python for Everybody - Specialisation\3_Using Python to Access Web Data\11_3_unnamed.txt").read())]))
+# print(sum([int(i) for i in re.findall("[0-9]+", open(r"...\11_3_unnamed.txt").read())]))
